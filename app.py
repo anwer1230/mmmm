95cedf7e15a4cbcfb3b714a8d15d8339de8b39cb
anwer1230 <anwrfwad178@gmail.com>
2025-09-05 18:31:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -298,4 +298,4 @@ async def continuous_sending():
             await asyncio.sleep(60)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
