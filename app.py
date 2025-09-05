91c6ca6137337612255cfb68ffdeac0146f84a08
anwer1230 <anwrfwad178@gmail.com>
2025-09-05 23:25:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ def api_stop_monitoring():
 # تشغيل التطبيق
 # -----------------------------
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
